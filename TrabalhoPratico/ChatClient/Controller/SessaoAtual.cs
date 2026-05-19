21ae42e43e49b6083824fff243162ecab8e65058
Diogo <diogo@example.com>
2026-05-19 19:18:50 +0100

    refactor: add ChatClient Model and Controller layers
    Create Model (Mensagem, DadosConexao) and Controller (SessaoAtual, GestorCliente, GestorConexao) following MVC pattern
    
    Ultraworked with Sisyphus (https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ TrabalhoPratico/ChatClient/Controller/SessaoAtual.cs
@@ -0,0 +1,8 @@
+namespace ChatClient
+{
+    public static class SessaoAtual
+    {
+        public static string Username { get; set; }
+        public static string IP { get; set; }
+    }
+}
---
+++ TrabalhoPratico/ChatClient/Model/Mensagem.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChatClient
+{
+    public class Mensagem
+    {
+        public string Remetente { get; set; }
+        public string Texto { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
